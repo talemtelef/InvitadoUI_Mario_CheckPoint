787bcb150472d95f4b67a4dd21a6313ffb3917d2
MarioSalmern <9waysofdestiny@gmail.com>
2017-05-26 14:08:14 +0200

    Copia de seguridad de la interfaz de invitado modelada gráficamente

---
+++ app/src/main/java/com/example/monitor/comunicacion/MainActivity.java
@@ -0,0 +1,192 @@
+package com.example.monitor.comunicacion;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView Ciudad;
+    TextView MinMax;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Ciudad = (TextView) this.findViewById(R.id.Ciudad);
+        MinMax = (TextView) this.findViewById(R.id.MinMax);
+    }
+
+
+    public void cargar(View v){
+        ComunicacionTask com=new ComunicacionTask();
+        Temperatura temperatura_asignacion = new Temperatura();
+
+
+        com.execute("http://www.aemet.es/xml/municipios/localidad_09059.xml"); //Burgos
+        temperatura_asignacion.execute("http://www.aemet.es/xml/municipios/localidad_09059.xml");
+
+    }
+
+
+    private class ComunicacionTask extends AsyncTask<String, Void, String> {
+
+
+        @Override
+        protected String doInBackground(String... params) {
+            String ciudad="";
+
+            try {
+                URL url = new URL(params[0]);
+                URLConnection con = url.openConnection();
+
+                InputStream is = con.getInputStream();
+                Document doc;
+                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder builder;
+                builder = factory.newDocumentBuilder();
+                doc = builder.parse(is);
+
+                //RECUPERAMOS LOS ELEMENTOS DEL API http://www.aemet.es/xml/municipios/localidad_09059.xml , EN ESTE CASO PARA BURGOS.
+                NodeList listanombre = doc.getElementsByTagName("nombre");
+                NodeList listaPrecip = doc.getElementsByTagName("prob_precipitacion");
+
+                NodeList listaTempMax = doc.getElementsByTagName("maxima");
+                NodeList listaTempMin = doc.getElementsByTagName("minima");
+
+                NodeList listaDireccViento = doc.getElementsByTagName("direccion");
+                NodeList listaVientoVelocidad = doc.getElementsByTagName("velocidad");
+
+
+                //MUESTRA DE LA LOCALIDAD SELECCIONADA:
+                ciudad += listanombre.item(0).getTextContent() + "\n";
+
+                /*
+
+                //CALCULAMOS LA MEDIA DE LAS PROBABILIDADES DE PRECIPITACIÓN, ASEGURANDONOS ASÍ QUE NO COJEMOS VALORES NULOS DE LOS DISTINTOS PERIODOS DE PROBABILIDAD.
+                double media_prob_precipitacion = 0;
+                for (int i = 0; i <= 6; i++) {
+                    String valor = listaPrecip.item(i).getTextContent();
+                    if (valor == null || valor.equals("")) {
+                        valor = "0";
+                    }
+                    media_prob_precipitacion += Double.parseDouble(valor);
+                }
+                media_prob_precipitacion = media_prob_precipitacion / 7;
+
+                //ASIGNAMOS LA MEDIA DE LAS PROB. DE PRECIPITACION AL TEXTVIEW.
+                cadena += "-Precipitaciones:" +
+                        (long)(media_prob_precipitacion > 0 ? media_prob_precipitacion + 0.01 : media_prob_precipitacion - 0.01) + "% " +"\n";
+
+
+                //Asignamos el viento:
+
+                cadena += "-Vientos:" + "\n" +
+                        "Dirección:  " +
+                        listaDireccViento.item(0).getTextContent() + "\n"+
+                        "Velocidad:  " +
+                        listaVientoVelocidad.item(0).getTextContent()+"km/h" + "\n" ;
+                */
+
+
+            }
+            catch(Exception ex){
+                ex.printStackTrace();
+            }finally {
+                return ciudad;
+            }
+
+
+        }
+
+        @Override
+        protected void onPostExecute(String result){
+
+            Ciudad.setText(result);
+
+
+        }
+
+    }
+
+
+    protected class Temperatura extends AsyncTask<String, Void, String> {
+
+
+        @Override
+        protected String doInBackground(String... params) {
+            String temperatura = "";
+            try {
+                URL url = new URL(params[0]);
+                URLConnection con = url.openConnection();
+
+                InputStream is = con.getInputStream();
+                Document doc;
+                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder builder;
+                builder = factory.newDocumentBuilder();
+                doc = builder.parse(is);
+
+                //RECUPERAMOS LOS ELEMENTOS DEL API http://www.aemet.es/xml/municipios/localidad_09059.xml , EN ESTE CASO PARA BURGOS.
+
+
+                NodeList listaTempMax = doc.getElementsByTagName("maxima");
+                NodeList listaTempMin = doc.getElementsByTagName("minima");
+
+                //CALCULAMOS TEMPERATURA MEDIA
+                double media_temp ;
+                double maximo = 0;
+                double minimo = 0;
+
+                    String valormax = listaTempMax.item(0).getTextContent();
+                    if (valormax == null || valormax.equals("")) {
+                        valormax = "0";
+                    }
+                    maximo += Double.parseDouble(valormax);
+                String valormin = listaTempMin.item(0).getTextContent();
+                if (valormin == null || valormin.equals("")) {
+                    valormin = "0";
+                }
+                minimo += Double.parseDouble(valormin);
+
+                media_temp = (maximo+minimo) / 2;
+
+
+                //ASIGNAMOS LA TEMPERATURA MEDIA, MAXIMA Y MINIMA AL TEXTVIEW.
+                temperatura += "  "+media_temp +"ºC"+ "\n" +
+                       "Max: "+ listaTempMax.item(0).getTextContent() + "ºC" + "\n"+
+                            "Min: " +   listaTempMin.item(0).getTextContent() + "ºC" + "\n";
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            } finally {
+                return temperatura;
+            }
+
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            MinMax.setText(result);
+
+        }
+
+    }
+
+
+
+
+
+}
